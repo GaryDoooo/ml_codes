40065a659d26f3e9d7d3e63ad5d5236792032a3f
GaryDoooo <gdu@gmx.us>
2023-06-25 04:01:25 -0400

    cron

--- haiwai_video_down_and_combine/if_extract_all_url.py
+++ haiwai_video_down_and_combine/if_extract_all_url.py
@@ -1,5 +1,5 @@
 import sys
-import re
+#  import re
 from playwright.sync_api import sync_playwright
 url = sys.argv[1]
 with sync_playwright() as p:
@@ -15,44 +15,27 @@ with sync_playwright() as p:
     for i in range(len(atxt)):
         if "?id=" not in aurl[i]:
             continue
+        if "play" not in aurl[i]:
+            continue
         try:
-            #  print(atxt[i], aurl[i])
-            e = int(atxt[i].replace(' ', ''))
+            print(atxt[i], aurl[i])
+            #  e = int(atxt[i].replace(' ', ''))
             #  print(e, aurl[i])
-            links.add((e, aurl[i]))
+            links.add((atxt[i], aurl[i]))
         except BaseException:
             pass
     #  print(ua)
     #  print(links)
     print("Found these episodes links:\n")
-    links = sorted(list(links), key=lambda x: x[0])
+    #  links = sorted(list(links), key=lambda x: x[0])
     #  print(links)
     for e, l in links:
         print(e, l)
     browser.close()
 
-s = input("Select episodes(e.g. 1,2,5-9 10 11):\n")
-picked = set()
-sl = re.split(r'; |, |\*|\n|,|;|\ ', s)
-#  print(sl)
-for i in sl:
-    try:
-        if '-' in i:
-            [a, b] = list(map(int, i.split('-')))
-            print(a, b)
-            if (a > b):
-                a, b = b, a
-            for j in range(a, b + 1):
-                picked.add(j)
-        else:
-            picked.add(int(i))
-    except BaseException:
-        print("Invalid episode", i)
-        pass
-
 for i in links:
-    if i[0] in picked:
-        print("Download", i[0], i[1])
+    #  if i[0] in picked:
+    print("Download", i[0], i[1])
 
 
 #  from bs4 import BeautifulSoup

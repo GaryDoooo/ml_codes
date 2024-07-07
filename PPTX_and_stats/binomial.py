4c6d0271099011bed5b160e288cfdc7f354467a9
GaryDoooo <gdu@gmx.us>
2024-07-07 04:01:30 -0400

    cron

--- PPTX_and_stats/binomial.py
+++ PPTX_and_stats/binomial.py
@@ -1,70 +1,80 @@
 from scipy.stats import binom
 
-def binomial(n,p,x=None,pvalue=None,print_out=True):
-    rv = binom(n,p)
-    res=dict()
-    
+
+def binomial(n, p, x=None, pvalue=None, print_out=False):
+    rv = binom(n, p)
+    res = dict()
+
     mean, var, skew, kurt = rv.stats(moments='mvsk')
-    stdev=var**.5
-    res["mean"],res["var"],res["skew"],res["kurt"],res["stdev"] = \
+    stdev = var**.5
+    res["mean"], res["var"], res["skew"], res["kurt"], res["stdev"] = \
         mean, var, skew, kurt, stdev
-    
+
     if print_out:
-        print("mean = %.2f\tvariance = %.2f\tstdev=%.2f"%(mean,var,stdev))
-        print("skewness coefficient = %.2f\tkurtosis coefficient = %.2f"%(skew,kurt))
-    
-    def acc_p(h,t,s,threshold):
-        p=0
-        for i in range(h,t,s):
-            p+=rv.pmf(i)
-            if p> threshold:
-                return p-rv.pmf(i), i-s
-            
-    if not x is None:
-        res["px"]=rv.pmf(x)
-        res["left_acc_p"]=sum([rv.pmf(i) for i in range(x+1)])
-        res["right_acc_p"]=sum([rv.pmf(i) for i in range(x,n+1)])
-        if res["left_acc_p"]<res["right_acc_p"]:
-            right_acc_p,right_x=acc_p(n,x,-1,res["left_acc_p"])
-            left_acc_p,left_x=res["left_acc_p"],x
+        print("mean = %.2f\tvariance = %.2f\tstdev=%.2f" % (mean, var, stdev))
+        print(
+            "skewness coefficient = %.2f\tkurtosis coefficient = %.2f" %
+            (skew, kurt))
+
+    def acc_p(h, t, s, threshold):
+        pp = 0
+        for i in range(h, t, s):
+            pp += rv.pmf(i)
+            if pp > threshold:
+                return pp - rv.pmf(i), i - s
+        print(h, t, s, threshold, n, p, x)
+        print(res["left_acc_p"], res["right_acc_p"])
+
+    if x is not None:
+        res["px"] = rv.pmf(x)
+        res["left_acc_p"] = sum([rv.pmf(i) for i in range(x + 1)])
+        res["right_acc_p"] = sum([rv.pmf(i) for i in range(x, n + 1)])
+        if res["left_acc_p"] < res["right_acc_p"]:
+            right_acc_p, right_x = acc_p(n, x - 1, -1, res["left_acc_p"])
+            left_acc_p, left_x = res["left_acc_p"], x
         else:
-            left_acc_p,left_x=acc_p(0,x,1,res["right_acc_p"])
-            right_acc_p,right_x=res["right_acc_p"],x
-        res["equal_tail"]={
-            "x_left":left_x,"x_right":right_x,
-            "acc_p_left":left_acc_p, "acc_p_right":right_acc_p
+            left_acc_p, left_x = acc_p(0, x + 1, 1, res["right_acc_p"])
+            right_acc_p, right_x = res["right_acc_p"], x
+        res["equal_tail"] = {
+            "x_left": left_x, "x_right": right_x,
+            "acc_p_left": left_acc_p, "acc_p_right": right_acc_p
         }
-        
+
         if print_out:
-            print("Prob at x = %d is %.3f."%(x,res["px"]))
-            print("Prob 0 to x including x total is %.3f."%res["left_acc_p"])
-            print("Prob x to n including x total is %.3f."%res["right_acc_p"])
-            print("Equal tail at two sides \nLeft at %d, acc. prob is %.3f."%(
-                    left_x,left_acc_p))
-            print("Right at %d, acc. prob is %.3f."%(right_x,right_acc_p))
-            
-    elif not pvalue is None:
-        res["left_acc_p"],res["x_left"]=acc_p(0,n+1,1,pvalue)
-        res["right_acc_p"],res["x_right"]=acc_p(n,-1,-1,pvalue)
-        left_acc_p,left_x=acc_p(0,n+1,1,pvalue/2)
-        right_acc_p,right_x=acc_p(n,-1,-1,pvalue/2)
-        res["equal_tail"]={
-            "x_left":left_x,"x_right":right_x,
-            "acc_p_left":left_acc_p, "acc_p_right":right_acc_p
+            print("Prob at x = %d is %.3f." % (x, res["px"]))
+            print("Prob 0 to x including x total is %.3f." % res["left_acc_p"])
+            print(
+                "Prob x to n including x total is %.3f." %
+                res["right_acc_p"])
+            print(
+                "Equal tail at two sides \nLeft at %d, acc. prob is %.3f." %
+                (left_x, left_acc_p))
+            print("Right at %d, acc. prob is %.3f." % (right_x, right_acc_p))
+
+    elif pvalue is not None:
+        res["left_acc_p"], res["x_left"] = acc_p(0, n + 1, 1, pvalue)
+        res["right_acc_p"], res["x_right"] = acc_p(n, -1, -1, pvalue)
+        left_acc_p, left_x = acc_p(0, n + 1, 1, pvalue / 2)
+        right_acc_p, right_x = acc_p(n, -1, -1, pvalue / 2)
+        res["equal_tail"] = {
+            "x_left": left_x, "x_right": right_x,
+            "acc_p_left": left_acc_p, "acc_p_right": right_acc_p
         }
-        
+
         if print_out:
-            print("P value = %.3f"%pvalue)
-            print("Prob 0 to x = %d including x total is %.3f."%
-                  (res["x_left"],res["left_acc_p"]))
-            print("Prob x = %d to n including x total is %.3f."%
-                  (res["x_right"],res["right_acc_p"]))
-            print("Equal tail at two sides \nLeft at %d, acc. prob is %.3f."%(
-                    left_x,left_acc_p))
-            print("Right at %d, acc. prob is %.3f."%(right_x,right_acc_p))
-        
-    res["pmf"]=[rv.pmf(i) for i in range(n+1)]
+            print("P value = %.3f" % pvalue)
+            print("Prob 0 to x = %d including x total is %.3f." %
+                  (res["x_left"], res["left_acc_p"]))
+            print("Prob x = %d to n including x total is %.3f." %
+                  (res["x_right"], res["right_acc_p"]))
+            print(
+                "Equal tail at two sides \nLeft at %d, acc. prob is %.3f." %
+                (left_x, left_acc_p))
+            print("Right at %d, acc. prob is %.3f." % (right_x, right_acc_p))
+
+    res["pmf"] = [rv.pmf(i) for i in range(n + 1)]
     return res
-        
-                    
-    
+
+
+if __name__ == "__main__":
+    binomial(250, .073, x=9, print_out=True)
--- PPTX_and_stats/proportion_test.py
+++ PPTX_and_stats/proportion_test.py
@@ -1,6 +1,54 @@
-from scipy.stats import norm
+from scipy.stats import norm, binom
+from scipy.stats import f as f_dist
+#  from functools import lru_cache
+from prettytable import PrettyTable as PT
+#  import numpy as np
 ####### Own Modules ########
 from binomial import binomial
+
+
+def p_blaker(n, p, k):
+    rv = binom(n, p)
+    psl = []
+    s = 0
+    for i in range(n + 1):
+        s += rv.pmf(i)
+        psl.append(s)
+
+    def pt_le_k(n, p, k):
+        return psl[k] if k >= 0 else 0
+
+    def pt_ge_k(n, p, k):
+        return 1 - pt_le_k(n, p, k - 1)
+
+    if pt_ge_k(n, p, k) < pt_le_k(n, p, k):
+        k_modified = max([i for i in range(n + 1)
+                          if pt_le_k(n, p, i) <= pt_ge_k(n, p, k)])
+        return pt_ge_k(n, p, k) + pt_le_k(n, p, k_modified)
+    elif pt_ge_k(n, p, k) == pt_le_k(n, p, k):
+        return 1
+    else:
+        k_modified = min([i for i in range(n + 1)
+                          if pt_ge_k(n, p, i) <= pt_le_k(n, p, k)])
+        return pt_le_k(n, p, k) + pt_ge_k(n, p, k_modified)
+
+
+def p_sterne(n, p, k):
+    rv = binom(n, p)
+    PTK = rv.pmf(k)
+    return sum([rv.pmf(i) for i in range(n + 1) if rv.pmf(i) <= PTK])
+
+
+def p_likelihood_ratio(n, p0, x):
+    p = x / n
+    rv = binom(n, p0)
+
+    def LR(i):
+        ii = min(i, n - i)
+        return (p / p0)**ii * ((1 - p) / (1 - p0))**(n - ii)
+
+    return sum(rv.pmf(y) for y in range(n + 1)
+               if LR(y) >= LR(x))
 
 
 def prop_test_1sample(events=1, N=1, p0=1,
@@ -10,6 +58,7 @@ def prop_test_1sample(events=1, N=1, p0=1,
     p_norm = (1 - norm.cdf(abs(Z))) * 2
     p_norm_less = norm.cdf(Z)
     b_res = binomial(N, p0, x=events, print_out=False)
+    #  print(b_res)
     p_bi = b_res["left_acc_p"]
 
     std = (p * (1 - p) / N)**.5
@@ -18,21 +67,73 @@ def prop_test_1sample(events=1, N=1, p0=1,
     CI_high = p + norm.ppf(1 - alpha) * std
     CI_low = p + norm.ppf(alpha) * std
 
+# https://support.minitab.com/en-us/minitab/help-and-how-to/statistics/basic-statistics/how-to/1-proportion/methods-and-formulas/methods-and-formulas/
+# Clopper-Pearson exact confidence interval method
+    dfn = 2 * events
+    dfd = 2 * (N - events + 1)
+    F = f_dist.ppf(alpha / 2, dfn, dfd)
+    p_exct_l = dfn * F / (dfd + dfn * F)
+    F = f_dist.ppf(alpha, dfn, dfd)
+    p_exct_low = dfn * F / (dfd + dfn * F)
+
+    dfn = 2 * (events + 1)
+    dfd = 2 * (N - events)
+    F = f_dist.ppf(1 - alpha / 2, dfn, dfd)
+    p_exct_u = dfn * F / (dfd + dfn * F)
+    F = f_dist.ppf(1 - alpha, dfn, dfd)
+    p_exct_high = dfn * F / (dfd + dfn * F)
+
+    p_s = p_sterne(N, p0, events)
+    p_b = p_blaker(N, p0, events)
+
     if print_out:
         print("One sample propotion test alpha = %.3f" % alpha)
-        print("Z porportion test")
-        print("p-value %.3f for real p equals p0." % p_norm)
-        print("p-value %.3f" % p_norm_less,
-              "for H0 p==p0, Ha p<p0.")
-        print("%%%.1f" % (100 - alpha * 100),
-              "CI (%.3f, %.3f), (0, %.3f), (%.3f, 1)" % (
-                  CI_l, CI_u, CI_high, CI_low))
-        print("\nBinomial test p-value %.3f" %
-              p_bi, "for gettig less or equal events if real p equals p0.")
-    return {"Z p": p_norm, "Binomial p": p_bi,
-            "Z p less": p_norm_less, "CI l": CI_l, "CI u": CI_u,
-            "CI high": CI_high, "CI low": CI_low}
+        #  print("Z porportion test")
+        print("\np-value for H0: p==p0, Ha: p!=p0")
+        print("Normal Approx.   %.3f" % p_norm)
+        print("Strerne's Method %.3f" % p_s)
+        print("Blaker's Method  %.3f" % p_b)
+        print("\nH0: p==p0, Ha: p<p0.")
+        print("Normal App. p-value = %.3f\nBinomial Exact p-value = %.3f" %
+              (p_norm_less, p_bi))
+        #  print("p-value %.3f" % p_norm_less,
+        #        "for H0 p==p0, Ha p<p0.")
+        pct = "%%%.1f" % (100 - alpha * 100)
+        #  "CI (%.3f, %.3f), (0, %.3f), (%.3f, 1)" % (
+        #      CI_l, CI_u, CI_high, CI_low))
+        #  print("\nBinomial test p-value %.3f" %
+        #        p_bi, "for gettig less or equal events if real p equals p0.")
+        t = PT()
+        t.field_names = ["N", "Event", "Sample P"]
+        t.add_row([str(N), str(events), "%.3f" % (events / N)])
+        print("")
+        print(t)
+        t = PT()
+        print("p-value CI", pct)
+        t.field_names = ["Algo", " CI", "Upper Bound",
+                         "Lower Bound"]
+        t.add_row(["Normal App.", "(%.3f, %.3f)" % (CI_l, CI_u),
+                   "%.3f" % CI_high, "%.3f" % CI_low])
+        t.add_row(["Clopper-Pearson\nexact", "(%.3f, %.3f)" %
+                   (p_exct_l, p_exct_u), "%.3f" %
+                   p_exct_high, "%.3f" %
+                   p_exct_low])
+        print(t)
+        print("")
+
+    return {"p norm": p_norm, "p Binomial": p_bi,
+            "Z p less": p_norm_less,
+            "CI norm": {"CI l": CI_l, "CI u": CI_u,
+                        "CI high": CI_high, "CI low": CI_low},
+            "p Sterne": p_s, "p Blaker": p_b,
+            "CI exact": {"CI l": p_exct_l, "CI u": p_exct_u,
+                         "CI high": p_exct_high, "CI low": p_exct_low}
+            }
 
 
 if __name__ == "__main__":
     print(prop_test_1sample(9, 250, .073, print_out=True))
+    print(p_blaker(250, .073, 9), p_sterne(250, .073, 9))
+    print(p_likelihood_ratio(250, .073, 9))
+    print(p_likelihood_ratio(50, .35, 10))
+    print(p_sterne(50, .35, 10))

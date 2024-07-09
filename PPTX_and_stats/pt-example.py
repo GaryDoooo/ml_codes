da25a8d8fcc0c2e177e12935475471ff3d7c182b
GaryDoooo <gdu@gmx.us>
2024-07-09 04:01:25 -0400

    cron

---
+++ PPTX_and_stats/pt-example.py
@@ -0,0 +1,29 @@
+from tkinter import *
+from pandastable import Table, TableModel, config
+
+
+class TestApp(Frame):
+    """Basic test frame for the table"""
+
+    def __init__(self, parent=None):
+        self.parent = parent
+        Frame.__init__(self)
+        self.main = self.master
+        self.main.geometry('600x400+200+100')
+        self.main.title('Table app')
+        f = Frame(self.main)
+        f.pack(fill=BOTH, expand=1)
+        df = TableModel.getSampleData()
+        self.table = pt = Table(f, dataframe=df,
+                                showtoolbar=True, showstatusbar=True)
+        pt.show()
+        # set some options
+        options = {'colheadercolor': 'green', 'floatprecision': 5}
+        config.apply_options(options, pt)
+        pt.show()
+        return
+
+
+app = TestApp()
+# launch the app
+app.mainloop()
